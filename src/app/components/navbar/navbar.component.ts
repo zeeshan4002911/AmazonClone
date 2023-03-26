4b080cea046f8819a5d7bc4fd95363cd6301b58b
Md Zeeshan <zeeshan.md@flutura.com>
2023-03-26 12:08:57 +0530

    search on enter click added

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import { SearchService } from 'src/app/services/search.service';
 
 @Component({
@@ -7,11 +8,16 @@ import { SearchService } from 'src/app/services/search.service';
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-  constructor(private search: SearchService) {}
+  constructor(private search: SearchService, private router: Router) {}
 
   ngOnInit(): void {}
 
   setSearch(input: string) {
     this.search.setSharedData(input);
+  }
+
+  onEnterClick(input: string) {
+    this.setSearch(input);
+    this.router.navigate(['/productList'])
   }
 }
